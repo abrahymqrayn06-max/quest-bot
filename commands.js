bcbd5242e9605929d18bbc3d65a9549ae5903715
abrahymqrayn06-max <abrahymqrayn06@gmail.com>
2026-06-10 17:52:50 +0300

    Create commands.js

---
+++ commands.js
@@ -0,0 +1,47 @@
+const { REST, Routes, SlashCommandBuilder } = require("discord.js");
+require("dotenv").config();
+
+const commands = [
+  new SlashCommandBuilder()
+    .setName("panel")
+    .setDescription("إرسال لوحة التذاكر")
+    .toJSON(),
+];
+
+const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
+
+(async () => {
+  try {
+    await rest.put(
+      Routes.applicationCommands(process.env.CLIENT_ID),
+      { body: commands }
+    );
+
+    console.log("تم تسجيل الأوامر بنجاح.");
+  } catch (err) {
+    console.error(err);
+  }
+})();const { REST, Routes, SlashCommandBuilder } = require("discord.js");
+require("dotenv").config();
+
+const commands = [
+  new SlashCommandBuilder()
+    .setName("panel")
+    .setDescription("إرسال لوحة التذاكر")
+    .toJSON(),
+];
+
+const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
+
+(async () => {
+  try {
+    await rest.put(
+      Routes.applicationCommands(process.env.CLIENT_ID),
+      { body: commands }
+    );
+
+    console.log("تم تسجيل الأوامر بنجاح.");
+  } catch (err) {
+    console.error(err);
+  }
+})();
